06461914d75d5d04b858ab511d52adccd4a9709a
MattHelton <mjoelhelton@me.com>
2019-12-17 20:32:20 -0500

    Lesson 16: Creates NoteCard component and maps over notes

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-
+import NoteCard from './NoteCard';
 class List extends Component {
   
   componentWillMount() {
     this.props.getNotes();
   }
 
-  render() { 
-    return ( 
+  render() {
+    const { notes } = this.props;
+
+    const cards = notes.map((note, index) => {
+      return(
+        <NoteCard
+          key={index}
+          index={index}
+          note={note}
+        />
+      );
+    });
+    return (
       <div className='list-container'>
-        List Component
+        {cards}
       </div>
      );
   }
